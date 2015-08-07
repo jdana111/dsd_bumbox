1d55882d69f912e5aad0f680c88a4077ce5b0fa8
John Dana <john@danasd.com>
2015-08-07 17:25:49 -0600

    Modified application template and route to point to fixture file.

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
+import albums from 'bumbox/models/album-fixtures';
 
 export default Ember.Route.extend({
-  model: function() {
+  model: function() {return albums;
   }
 });
